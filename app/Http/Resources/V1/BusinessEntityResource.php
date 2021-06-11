f17083f9cf41aff0cdc3539a557842c09fcae270
Franzua Andrez <edson.andrez@outlook.es>
2021-06-10 21:31:56 -0600

    BusinessEntityResource added.

---
+++ app/Http/Resources/V1/BusinessEntityResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\v1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BusinessEntityResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'business_entity_id' => $this->business_entity_id,
+            'email_addresses' => $this->emailAddress,
+            'addresses' => BusinessEntityAddressResource::collection($this->businessEntityAddress)
+        ];
+    }
+}
