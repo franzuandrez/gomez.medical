c078aaec7c3ed3f17a9443d7e078de747334e9d4
Franzua Andrez <edson.andrez@outlook.es>
2021-06-11 15:13:32 -0600

    Product Subcategory crud.

---
+++ app/Http/Controllers/Api/V1/ProductSubCategoryController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\v1\ProductSubCategoryResource;
+use App\Models\ProductSubcategory;
+use Illuminate\Http\Request;
+
+class ProductSubCategoryController extends Controller
+{
+    //
+
+
+    public function index(): \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+    {
+
+        $subcategories = ProductSubcategory::orderBy('updatedAt', 'desc')
+            ->paginate(15);
+
+
+        return ProductSubCategoryResource::collection($subcategories);
+
+    }
+
+
+    public function store(Request $request): ProductSubCategoryResource
+    {
+
+        $subcategory = new ProductSubcategory();
+        $subcategory->name = $request->get('name');
+        $subcategory->product_category_id = $request->get('product_category_id');
+        $subcategory->save();
+
+
+        return new ProductSubCategoryResource($subcategory);
+    }
+
+
+    public function show($id): ProductSubCategoryResource
+    {
+
+        $subcategory = ProductSubcategory::findOrFail($id);
+
+
+        return new ProductSubCategoryResource($subcategory);
+    }
+
+    public function update(Request $request, $id): ProductSubCategoryResource
+    {
+
+
+        $subcategory = ProductSubcategory::findOrFail($id);
+        $subcategory->name = $request->get('name');
+        $subcategory->product_category_id = $request->get('product_category_id');
+        $subcategory->update();
+
+
+        return new ProductSubCategoryResource($subcategory);
+    }
+
+
+    public function destroy($id)
+    {
+        try {
+
+            $subcategory = ProductSubcategory::findOrFail($id);
+
+            $subcategory->delete();
+
+            return response("", 204);
+
+        } catch (\Exception $exception) {
+
+            return response($exception->getMessage(), 500);
+        }
+
+    }
+}
