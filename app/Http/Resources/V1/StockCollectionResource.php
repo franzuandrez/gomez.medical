eaa77b0d2ac1a75d04f269bed53f2a6b6f25ce00
Franzua Andrez <edson.andrez@outlook.es>
2021-08-07 15:11:16 -0600

    v1 StockCollectionResource.php and InventoryResource.php

--- app/Http/Resources/V1/StockCollectionResource.php
+++ app/Http/Resources/V1/StockCollectionResource.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Resources\v1;
+namespace App\Http\Resources\V1;;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class StockCollectionResource extends JsonResource
@@ -9,9 +10,10 @@ class StockCollectionResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param Request $request
      * @return array
      */
+
     public function toArray($request)
     {
         return [
