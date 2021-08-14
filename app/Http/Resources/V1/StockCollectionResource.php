5ff86f4a4cc31df73fbcf8d027a24b3d308ef59f
Franzua Andrez <edson.andrez@outlook.es>
2021-08-14 13:09:54 -0600

    Stock id added.

--- app/Http/Resources/V1/StockCollectionResource.php
+++ app/Http/Resources/V1/StockCollectionResource.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace App\Http\Resources\V1;;
+namespace App\Http\Resources\V1;
+;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -17,13 +18,14 @@ class StockCollectionResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->product_id . '-' . $this->batch,
             'batch' => $this->batch,
             'best_before' => $this->best_before,
             'bin' => $this->bin,
             'sku' => $this->sku,
             'product_id' => $this->product_id,
             'code' => $this->code,
-            'name' => $this->name,
+            'name' => $this->name,  
             'description' => $this->description,
             'color' => $this->color,
             'size' => $this->size,
