6678a02201dce06308b39a9a5935eb140e85b4a2
franzua <edson.andrez@outlook.es>
2021-12-03 12:01:51 -0600

    clear string function for skus generation

--- app/Http/Controllers/Api/V1/PrintoutController.php
+++ app/Http/Controllers/Api/V1/PrintoutController.php
@@ -5,12 +5,33 @@ namespace App\Http\Controllers\api\v1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\v1\PrintoutCollectionResource;
 use App\Models\Printout;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class PrintoutController extends Controller
 {
     //
+
+    private function clear($name)
+    {
+
+        $name = str_replace(['-', '+'], '', $name);
+        $name = str_replace('Á', 'A', $name);
+        $name = str_replace('á', 'a', $name);
+        $name = str_replace('Ñ', 'N', $name);
+        $name = str_replace('ñ', 'n', $name);
+        $name = str_replace('É', 'E', $name);
+        $name = str_replace('é', 'e', $name);
+        $name = str_replace('Í', 'I', $name);
+        $name = str_replace('í', 'i', $name);
+        $name = str_replace('Ó', 'O', $name);
+        $name = str_replace('ó', 'o', $name);
+        $name = str_replace('Ú', 'U', $name);
+        return str_replace('ú', 'u', $name);
+    }
+
+
 
 
     public function show($id): AnonymousResourceCollection
@@ -25,11 +46,11 @@ class PrintoutController extends Controller
             'printouts.quantity',
             'printouts.is_printed',
         )
-        ->where('doc_id', $id)
-            ->join('product','product.product_id','=','printouts.product_id')
+            ->where('doc_id', $id)
+            ->join('product', 'product.product_id', '=', 'printouts.product_id')
             ->paginate(15);
 
-        return   PrintoutCollectionResource::collection($prints);
+        return PrintoutCollectionResource::collection($prints);
 
     }
 }
