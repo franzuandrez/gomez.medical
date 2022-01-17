bbba1fd348ae53cb1e3b4c723b34bea064c48f1a
franzua <edson.andrez@outlook.es>
2022-01-16 18:46:34 -0600

    remove unprotected get all banks route

--- routes/api.php
+++ routes/api.php
@@ -74,7 +74,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('v1/banks', [\App\Http\Controllers\Api\V1\BankController::class,'index']);
     Route::post('v1/business_entity_banks', [\App\Http\Controllers\Api\V1\BusinessEntityAddBankAccountController::class,'store']);
 });
-Route::get('v1/banks', [\App\Http\Controllers\Api\V1\BankController::class,'index']);
+
 Route::get('v1/labels_types',[\App\Http\Controllers\api\v1\LabelTypeController::class,'index']);
 Route::get('v1/inventory_printouts',[\App\Http\Controllers\api\v1\InventoryPrintoutController::class,'index']);
 Route::get('v1/pending_printouts/{id}', [\App\Http\Controllers\api\v1\PrintoutController::class, 'show']);
