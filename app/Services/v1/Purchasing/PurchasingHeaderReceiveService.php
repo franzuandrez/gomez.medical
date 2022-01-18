913e08f8d7d7fca35fe97f2ec310b7e6a3e91566
franzua <edson.andrez@outlook.es>
2022-01-18 15:12:09 -0600

    add PayInPayOut when a sale is generated and when a purchase is received

--- app/Services/v1/Purchasing/PurchasingHeaderReceiveService.php
+++ app/Services/v1/Purchasing/PurchasingHeaderReceiveService.php
@@ -5,9 +5,13 @@ namespace App\Services\v1\Purchasing;
 
 
 use App\DTOs\v1\BaseAbstractDto;
+use App\DTOs\v1\PayInPayOut\PayInPayOutCreateDto;
 use App\DTOs\v1\Purchasing\PurchasingHeaderReceiveDto;
+use App\Models\PaymentType;
 use App\Models\PurchaseOrderHeader;
+use App\Services\v1\PayInPayOut\PayInPayOutCreateService;
 use App\Services\v1\ServiceInterface;
+use Illuminate\Support\Carbon;
 use InvalidArgumentException;
 
 class PurchasingHeaderReceiveService implements ServiceInterface
@@ -43,10 +47,34 @@ class PurchasingHeaderReceiveService implements ServiceInterface
         $header->ship_method_id = $this->dto->getShipMethodId();
         $header->subtotal = $this->dto->getSubtotal();
         $header->freight = $this->dto->getFreight();
+
+        if ($header->freight > 0) {
+            $this->pay_in_pay_out_shipping($header);
+        }
         $header->total_due = $header->freight + $header->subtotal;
         $header->markAsReceived();
         $header->update();
 
         return $header->toArray();
+    }
+
+
+    private function pay_in_pay_out_shipping($purchase)
+    {
+        $pay_in_pay_out_values['doc_type'] = 'SHIPPING';
+        $pay_in_pay_out_values['doc_id'] = $purchase->purchase_order_id;
+        $pay_in_pay_out_values['amount'] = $purchase->freight;
+        $pay_in_pay_out_values['pay_date'] = Carbon::now();
+        $pay_in_pay_out_values['description'] = 'Pago de envio ';
+        $pay_in_pay_out_values['payment_type_id'] = PaymentType::where('internal_code', 'cash')->first()->id;//TODO get payment_type
+        $pay_in_pay_out_values['comments'] = null;
+        $pay_in_pay_out_values['factor'] = -1;
+        $pay_in_pay_out_values['cash_register_id'] = 1;//TODO get cash_register;
+        $pay_in_pay_out_values['employee_id'] = $purchase->employee_id;
+
+        $dto = new PayInPayOutCreateDto($pay_in_pay_out_values);
+        $service = PayInPayOutCreateService::make($dto);
+        $service->execute();
+
     }
 }
--- app/Services/v1/Sales/SalesHeaderCreateService.php
+++ app/Services/v1/Sales/SalesHeaderCreateService.php
@@ -5,10 +5,13 @@ namespace App\Services\v1\Sales;
 
 
 use App\DTOs\v1\BaseAbstractDto;
+use App\DTOs\v1\PayInPayOut\PayInPayOutCreateDto;
 use App\DTOs\v1\Sales\SalesHeaderCreateDto;
 use App\Models\Employee;
+use App\Models\PaymentType;
 use App\Models\SalesOrderHeader;
 use App\Models\SalesPerson;
+use App\Services\v1\PayInPayOut\PayInPayOutCreateService;
 use App\Services\v1\ServiceInterface;
 use Carbon\Carbon;
 use InvalidArgumentException;
@@ -41,7 +44,7 @@ class SalesHeaderCreateService implements ServiceInterface
 
         $sales_header = new SalesOrderHeader();
 
-
+        $cash_register_id = 1;
         $sales_header->online_order_flag = $this->dto->getOnlineOrderFlag();
         $sales_header->order_date = Carbon::now();
         $sales_header->sales_order_number = Str::uuid()->toString();
@@ -53,17 +56,44 @@ class SalesHeaderCreateService implements ServiceInterface
         $sales_header->subtotal = $this->dto->getSubtotal();
         $sales_header->freight = $this->dto->getFreight();
         $sales_header->total_due = $this->dto->getTotalDue();
-        $sales_header->cash_register_id = 1;// TODO get current cash register
+        $sales_header->cash_register_id = $cash_register_id;// TODO get current cash register
         $sales_header->comments = $this->dto->getComments();
+        $employee = Employee::whereLoginId(\Auth::id())->first();
+
         $sales_header->sales_person_id = SalesPerson::whereBusinessEntityId(
-            Employee::whereLoginId(\Auth::id())->first()->business_entity_id
-        )->sales_person_id;
+            $employee->business_entity_id
+        )->first()->sales_person_id;
         $sales_header->save();
         if ($this->dto->getIsPaid()) {
             $sales_header->markAsPaid();
+            $this->pay_in_pay_out_sale($sales_header, $employee);
         }
         $sales_header->markAsInProcess();
+
+
         return $sales_header->toArray();
 
     }
+
+
+    private function pay_in_pay_out_sale($sale, $employee)
+    {
+        $pay_in_pay_out_values['doc_type'] = 'SALE';
+        $pay_in_pay_out_values['doc_id'] = $sale->sales_order_id;
+        $pay_in_pay_out_values['amount'] = $sale->total_due;
+        $pay_in_pay_out_values['pay_date'] = $sale->order_date;
+        $pay_in_pay_out_values['description'] = 'Venta generada';
+        $pay_in_pay_out_values['comments'] = null;
+        $pay_in_pay_out_values['factor'] = 1;
+        $pay_in_pay_out_values['factor'] = 1;
+        $pay_in_pay_out_values['cash_register_id'] = $sale->cash_register_id;
+        $pay_in_pay_out_values['employee_id'] = $employee->employee_id;
+        $pay_in_pay_out_values['payment_type_id'] = PaymentType::where('internal_code', $sale->payment_type)->id;//TODO get id
+
+        $dto = new PayInPayOutCreateDto($pay_in_pay_out_values);
+        $service = PayInPayOutCreateService::make($dto);
+        $service->execute();
+
+    }
+
 }
