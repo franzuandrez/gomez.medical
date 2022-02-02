aba84628ed04332433ca31680c59caa844408daf
franzua <edson.andrez@outlook.es>
2022-02-01 21:45:23 -0600

    PurchasePayment added.

---
+++ app/Models/PurchasePayment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PurchasePayment extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'payment_date',
+        'purchase_id',
+        'payment_type_id',
+        'employee_id',
+        'amount',
+        'doc_number_reference',
+        'observations',
+    ];
+}
