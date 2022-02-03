0d9809f7f49c441b4dc371105009c929db90de63
franzua <edson.andrez@outlook.es>
2022-02-02 23:52:52 -0600

    ListPriceHistoryController added.

---
+++ app/Http/Controllers/Api/V1/ListPriceHistoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ListPriceHistoryController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+}
