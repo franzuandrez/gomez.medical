2b8642f130f4d0c636aa12617bb288764191e127
franzua <edson.andrez@outlook.es>
2022-02-11 23:23:21 -0600

    Permissions Library Added

---
+++ app/DTOs/v1/Sales/SalesPaymentCreateDto.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DTOs\v1\Sales;
+
+use App\DTOs\v1\BaseAbstractDto;
+
+class SalesPaymentCreateDto extends BaseAbstractDto
+{
+
+
+
+    protected function configureValidatorRules(): array
+    {
+        // TODO: Implement configureValidatorRules() method.
+    }
+
+    protected function map(array $data): bool
+    {
+        // TODO: Implement map() method.
+    }
+}
